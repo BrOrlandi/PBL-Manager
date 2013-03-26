7ffb243c3168fb560cd9b7fa5e9ba89d8eeda986
BrOrlandi <brorlandi@gmail.com>
2013-03-26 12:52:46 -0300

    Cadastro de usuários e cadastro de tarefas perfeito sendo armazenados localmente.

--- pbl-manager/js/core.js
+++ pbl-manager/js/core.js
@@ -31,10 +31,13 @@ var Problema = function(titulo, descricao){
 //	this.usuarios = new Array();
 }
 
-var Tarefa = function(titulo, descricao, status){
+var Tarefa = function(id, titulo, descricao, status, data_entrega){
+	this.id = id;
 	this.titulo = titulo;
 	this.descricao = descricao;
 	this.status = status;
+	this.data_criacao = $.now();
+	this.data_entrega = data_entrega;
 }
 
 var Usuario = function(id,nome,email){
@@ -44,40 +47,35 @@ var Usuario = function(id,nome,email){
 }
 
 var cadastrarUsuario = function(nome, email){
-	var users = loadUsuarios();
-	var id;
-	if(users.length == 0){
-		id = 0;
-	}
-	else{
-		var last = users[users.length-1];
-		id = last.id + 1;	
-	}
+	var users_data = loadUsuarios();
 	
-	var new_user = new Usuario(id,nome,email);
+	users_data.lastID++;
 	
-	users.push(new_user);
-	users.sort(function(a,b){
+	var new_user = new Usuario(users_data.lastID,nome,email);
+	
+	users_data.users.push(new_user);
+	users_data.users.sort(function(a,b){
 		var username_a = a.nome;
 		var username_b = b.nome;
 		return username_a.localeCompare(username_b);
 	});
-	saveUsers(users);
+	saveUsers(users_data);
 }
 
 var removeUser = function(id){
-	var users = loadUsuarios();
+	var users_data = loadUsuarios();
 	//var find = $.inArray(value, array);
-	var find = $.grep(users, function(e){
-		return e.id;
+	var find = $.grep(users_data.users, function(e){
+		return e.id == id;
 	});
 	if(find.length == 0){
+		//alert('Not Found');
 		return;
 	}
 	var user_remove = find[0];
-	var index = users.indexOf(user_remove,0);
-	users.splice(index,1);
-	saveUsers(users);
+	var index = users_data.users.indexOf(user_remove,0);
+	users_data.users.splice(index,1);
+	saveUsers(users_data);
 }
 
 var loadUsuarios = function(){
@@ -86,10 +84,12 @@ var loadUsuarios = function(){
 	
 	var users = window.localStorage.getItem("usuarios");
 	if(users == null){
+		var users_data = new Object();
 		var users_new = new Array();
-		var json_users = window.JSON.stringify(users_new);
-		window.localStorage.setItem("usuarios",json_users);
-		users_return = users_new;
+		users_data.lastID = -1;
+		users_data.users = users_new;
+		saveUsers(user_data);
+		users_return = users_data;
 	}
 	else
 	{
@@ -101,4 +101,70 @@ var loadUsuarios = function(){
 var saveUsers = function(users){
 	var json = window.JSON.stringify(users);
 	window.localStorage.setItem("usuarios",json);
+}
+
+var cadastrarTarefa = function(titulo, desc,status,data_entrega){
+	var tarefas_data = loadTarefas();
+	
+	tarefas_data.lastID++;
+
+	var new_tarefa = new Tarefa(tarefas_data.lastID,titulo,desc,status,data_entrega);
+	
+	tarefas_data.tarefas.push(new_tarefa);
+	tarefas_data.tarefas.sort(function(a,b){
+		var status_a = a.status;
+		var status_b = b.status;
+		var dif = status_a - status_b;
+		if(dif != 0)
+		{
+			return dif;
+		}
+		else
+		{
+			return a.data_criacao - b.datacriacao;
+		}
+	});
+	saveTarefas(tarefas_data);
+}
+
+var removeTarefa = function(id){
+	var tarefas_data = loadTarefas();
+	//var find = $.inArray(value, array);
+	var find = $.grep(tarefas_data.tarefas, function(e){
+		return e.id == id;
+	});
+	if(find.length == 0){
+		//alert('Not Found');
+		return;
+	}
+	var tarefa_remove = find[0];
+	var index = tarefas_data.tarefas.indexOf(tarefa_remove,0);
+	tarefas_data.tarefas.splice(index,1);
+	saveTarefas(tarefas_data);
+}
+
+
+var loadTarefas = function(){
+	
+	var tarefas_return;
+	
+	var tarefas = window.localStorage.getItem("tarefas");
+	if(tarefas == null){
+		var tarefas_data = new Object();
+		var tarefas_new = new Array();
+		tarefas_data.lastID = -1;
+		tarefas_data.tarefas = tarefas_new;
+		saveTarefas(tarefas_data);
+		tarefas_return = tarefas_data;
+	}
+	else
+	{
+		tarefas_return = window.JSON.parse(tarefas);
+	}
+	return tarefas_return;
+}
+
+var saveTarefas = function(tarefas){
+	var json = window.JSON.stringify(tarefas);
+	window.localStorage.setItem("tarefas",json);
 }
--- pbl-manager/js/script.js
+++ pbl-manager/js/script.js
@@ -5,13 +5,12 @@ var prob = getProblema();
 
 $('#problema').html(prob.titulo);
 $('#prob_desc').html(prob.descricao);
-$('#n_tarefas').html(prob.tarefas.length);
 
-var users = loadUsuarios();
+var users_data = loadUsuarios();
 
-$('#n_usuarios').html(users.length);
+$('#n_usuarios').html(users_data.users.length);
 
-users.forEach(function(user){
+users_data.users.forEach(function(user){
 	$('#lista_usuarios').append('<li user_id="'+user.id+'"><a href="#">'+user.nome+'<p></p><p>'+user.email+'</p></a></li>');
 });
 $('#lista_usuarios').listview('refresh');
@@ -23,6 +22,73 @@ $('#button_cadastrar_usuario').click(function() {
 	cadastrarUsuario(nome,email);
 });
 
-//removeUser(1);
+
+var tarefas_data = loadTarefas();
+
+$('#n_tarefas').html(tarefas_data.tarefas.length);
+tarefas_data.tarefas.forEach(function(tarefa){
+	$('#lista_tarefas').append('<li tarefa_id="'+tarefa.id+'" class="'+ applyClass(tarefa) +'"><a href="#">'+tarefa.titulo+'<p></p><p>'+tarefa.descricao+'</p><p class="ui-li-aside">'+formatTarefaStatus(tarefa.status)+'<br/>'+formatTarefaDataEntrega(tarefa.data_entrega)+'</p></a></li>');
+});
+$('#lista_tarefas').listview('refresh');
+
+$('#button_cadastrar_tarefa').click(function() {
+	var nome = $('#nome_tarefa').val();
+	var desc = $('#desc_tarefa').val();
+	var status = $('#status_tarefa').val();
+	var data = $('#data_entrega_tarefa').val();
+	cadastrarTarefa(nome,desc,status,data);
+});
 
 });
+
+
+var applyClass = function(tarefa){
+	var status = tarefa.status;
+
+	if (status == Status.Pendente) {
+		return 'pendente';
+	} else if (status == Status.EmAndamento) {
+		return 'emandamento';
+	} else if (status == Status.Concluido) {
+		return 'concluido';
+	}
+
+}
+
+var formatTarefaStatus = function(status){	
+	if(status == Status.Pendente){
+		return 'Pendente';
+	}
+	else if(status == Status.EmAndamento){
+		return 'Em andamento';
+	}
+	else if(status == Status.Concluido){
+		return 'Concluído';
+	}
+}
+
+/*
+var formatTarefaStatus = function(status){	
+	if(status == Status.Pendente){
+		return '<span style=" color: #0097dd">Pendente</span>';
+	}
+	else if(status == Status.EmAndamento){
+		return '<span style=" color: #f7b500">Em andamento</span>';
+	}
+	else if(status == Status.Concluido){
+		return '<span style=" color: #00e500">Concluido</span>';
+	}
+}
+*/
+
+
+var formatTarefaDataEntrega= function(data){
+	var date = new Date(data);
+	//return date.toLocaleDateString();
+	var day = date.getDate();
+	var day_f = day > 9? day : '0'+day;
+	var month = date.getMonth() +1;
+	var month_f = month > 9? month : '0'+month;
+	return date.toDateString(); 
+	//return  day_f + '/' + month_f;
+}
